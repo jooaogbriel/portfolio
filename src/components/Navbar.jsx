928f61d39ca506ddb4a9984dfbb33cb7ae6123c5
João Gonçalves <joao_i0807@outlook.com>
2023-03-08 21:14:07 -0300

    feat: initial Navbar complet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,9 +29,9 @@ const Navbar = () => {
             <img src={logo} alt="logo" className='w-9
             h-9 object-contain' />
             <p className='text-white text-[18px]
-            font-bold cursor-pointer'>João<span
-            className='sm:block hidden'  > | JavaScript 
-            Mastery </span></p>
+            font-bold cursor-pointer flex'>João &nbsp;<span
+            className='sm:block hidden'>| Front-end Dev
+            </span></p>
           </Link>
           <ul className='list-none hidden sm:flex
           flex-row gap-10'>
@@ -63,6 +63,27 @@ const Navbar = () => {
                : 'flex'} p-6 black-gradient absolute
                top-20 right-0 mx-4 my-2 min-w-[140px]
                z-10 rounded-xl `}>
+
+                <ul className='list-none flex justify-end
+                 items-start flex-col gap-4'>
+                    {navLinks.map((link) => (
+                      <li
+                        key={link.id}
+                        className={`${
+                          active === link.title
+                            ? "text-white"
+                            : "text-secondary"
+                        }font-poppins font-medium
+                        cursor-pointer text-[16px]`}
+                        onClick={()=> {
+                          setToggle(!toggle)
+                          setActive(link.title)
+                        }}
+                      >
+                        <a href={`#${link.id}`}>{link.title}</a>
+                      </li>
+                    ))}
+          </ul>
 
               </div>
           </div>
