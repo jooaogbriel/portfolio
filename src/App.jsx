d56aa9bf9de4daf996397b712ae194129e82b65b
João Gonçalves <joao_i0807@outlook.com>
2023-03-17 15:12:57 -0300

    fix: install libs from react

--- src/App.jsx
+++ src/App.jsx
@@ -28,3 +28,25 @@ const App = () => {
 }
 
 export default App
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5XCPHvLgmCr2IW7YgYFGx31WSh02oZ4Q",
+  authDomain: "portfolio-a7b66.firebaseapp.com",
+  projectId: "portfolio-a7b66",
+  storageBucket: "portfolio-a7b66.appspot.com",
+  messagingSenderId: "688614513021",
+  appId: "1:688614513021:web:4159c6c18fcde52c051414",
+  measurementId: "G-ZXL20QFWVG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
